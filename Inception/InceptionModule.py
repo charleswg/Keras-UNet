323c9f8078028b56220a582654892c7146127ad3
charleswg <charleswghk@gmail.com>
2021-02-11 17:36:44 -0600

    Update InceptionModule.py

--- Inception/InceptionModule.py
+++ Inception/InceptionModule.py
@@ -8,8 +8,17 @@ from keras_gcnn.layers.pooling import GroupPool
 from groupy.gconv.tensorflow_gconv.splitgconv2d import gconv2d_util, gconv2d
 
 from keras.layers import Convolution2D,Activation, BatchNormalization,MaxPooling2D, concatenate
-def InceptionModule(inputs, numFilters = 32):
+def InceptionModule(self, inputs, numFilters = 32, h_input=None, h_output=None):
     
+    if h_input is None:
+        h_input= self.conv_group
+    if h_output is None:
+        h_output= self.conv_group
+        
+    down0 = GConv2D(f, (3, 3), padding='same',kernel_initializer = 'he_normal',h_input='Z2',h_output=conv_group,bias_initializer=Constant(0.0001), kernel_regularizer=l2(weight_decay))(inputs)
+    down0 = GBatchNorm(conv_group,trainable=self.trainable)(down0)
+    
+    tower_0 = GConv2D(numFilters, (3, 3), padding='same',kernel_initializer = 'he_normal',h_input='Z2',h_output=conv_group,bias_initializer=Constant(0.0001), kernel_regularizer=l2(weight_decay))(inputs)
     tower_0 = Convolution2D(numFilters, (1,1), padding='same', kernel_initializer = 'he_normal')(inputs)
     tower_0 = BatchNormalization()(tower_0)
     tower_0 = Activation("relu")(tower_0)
